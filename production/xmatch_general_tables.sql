61cfcd8101481a5f1cdf269940194bec9741ed4f
mshira <mshira@cambium.co.il>
2015-06-03 12:10:59 +0300

    test

--- production/xmatch_general_tables.sql
+++
@@ -1,1 +0,0 @@
-mysqldump: Got error: 1045: Access denied for user 'xmatchge_rami'@'localhost' (using password: YES) when trying to connect
--- pullPush.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo shell_exec("/usr/bin/git commit");
-echo 1;
-echo shell_exec("/usr/bin/git pull https://mshira:shira6847@github.com/ramiflam/xmatchge");
-echo 2;
-echo shell_exec("/usr/bin/git push https://mshira:shira6847@github.com/ramiflam/xmatchge");
-echo 3;
-?>
