3384cd7f32f97c033f0399a36b7e6be3483d26dd
ramiflam <xmatchge@box1153.bluehost.com>
2015-06-03 02:36:47 -0600

    fix sort by name

--- developmant/filename.sql
+++
@@ -1,1 +0,0 @@
-Enter password: mysqldump: Got error: 1045: Access denied for user 'xmatchge_T1'@'localhost' (using password: NO) when trying to connect
--- developmant/install.sql
+++
@@ -1,4 +0,0 @@
-create database nom;
-
-use nom;
-source  C:/Users/USER/Downloads/xmatch_general_a.sql;
